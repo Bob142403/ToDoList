e4f83cf88eece6f0b58eda49d8f8798a0cb1314b
Bob142403 <boboshka2006@mail.ru>
2023-01-06 23:08:42 +0300

    test

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ function saveItem() {
   let txt = div.querySelector(".textitem");
 
   txt.innerHTML = inputtxt.value;
+  if(!inputtxt.value) div.remove();
   inputtxt.value = "";
   addbtn.innerHTML = "Add Item";
+  addbtn.style.borderRadius = "0 5px 5px 0";
+  cancel.style.display = "none";
+
 
   addbtn.removeEventListener("click", saveItem);
   addbtn.addEventListener("click", addItem);
 }
-
 
 function addItem() {
   if (inputtxt.value) {
@@ -31,13 +34,16 @@ function addItem() {
 
     btndel.onclick = (event) => {
       event.target.parentNode.remove();
-      
-      if(!list.innerHTML) clearblock.style.display = "none";
+
+      if (!list.innerHTML) clearblock.style.display = "none";
     };
     btnedit.onclick = (event) => {
       doid = event.target.parentNode.parentNode.id;
       inputtxt.value = text.innerHTML;
+      prevtextvalue = inputtxt.value;
       addbtn.innerHTML = "Save Item";
+      cancel.style.display = "";
+      addbtn.style.borderRadius = "0";
       addbtn.removeEventListener("click", addItem);
       addbtn.addEventListener("click", saveItem);
     };
@@ -53,15 +59,24 @@ function clearlist() {
   iditem = 0;
 }
 
-let iditem = 0, doid = 0;
+function changevalue() {
+  inputtxt.value = prevtextvalue;
+}
+
+let iditem = 0,
+  doid = 0;
+let prevtextvalue = "";
 let mainlist = document.getElementById("mainList");
 let addbtn = document.getElementById("add");
 let inputtxt = document.getElementById("txt");
 let list = document.getElementById("List");
 let clearbtn = document.getElementById("clear");
 let clearblock = document.getElementById("clearblock");
+let cancel = document.getElementById("cancel");
 
 clearblock.style.display = "none";
+cancel.style.display = "none";
 
 addbtn.addEventListener("click", addItem);
 clearbtn.addEventListener("click", clearlist);
+cancel.addEventListener("click", changevalue)
